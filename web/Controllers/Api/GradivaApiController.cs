ca171773e9c9e4237031c39309c6f302d813532f
Timotej Blazic <timi.blazic@gmail.com>
2022-01-04 19:28:54 +0100

    GradivaApi Fixed

--- web/Controllers/Api/GradivaApiController.cs
+++ web/Controllers/Api/GradivaApiController.cs
@@ -10,7 +10,7 @@ using web.Models;
 
 namespace web.Controllers_Api
 {
-    [Route("api/v1/gradiva")]
+    [Route("api/[controller]")]
     [ApiController]
     public class GradivaApiController : ControllerBase
     {
